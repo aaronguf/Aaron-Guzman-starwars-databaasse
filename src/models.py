dabebca7316a84e944b6cfdf134ce9402f74e647
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2024-01-18 22:39:02 +0000

    Data modeling for SW Blog, done

--- src/models.py
+++ src/models.py
@@ -1,29 +1,88 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import DateTime
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# Many to Many
+favorited_vehicles=Table('favorite_vehicles',Base.metadata,
+                        Column('user_id',Integer,(ForeignKey('user.id'))),
+                        Column('vehicles_id',Integer,(ForeignKey('vehicles.id')))
+                         )
+
+class FavoriteCharacter(Base):
+    __tablename__='favorite_characters'
+    id=Column(Integer,primary_key=True)
+    created_at=Column(DateTime,default=datetime.utcnow)
+
+    user_id=Column(Integer,ForeignKey('user.id'))
+    user=relationship('User',back_populates='favorited_by')
+    character_id=Column(Integer,ForeignKey('character.id'))
+    character=relationship('Characters',back_populates='favorite_characters')
+
+
+# Explicit Association Model
+class FavoritePlanets(Base):
+    __tablename__='favorite_planets'
+    id=Column(Integer,primary_key=True)
+    created_at=Column(DateTime,default=datetime.utcnow)
+
+
+    user_id=Column(Integer,ForeignKey('user.id'))
+    user=relationship('User',back_populates='favorited_by')
+
+    planets_id=Column(Integer,ForeignKey('planets.id'))
+    planets=relationship('Planets',back_populates='favorite_planets')
+
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_status=Column(String(250), nullable=False)
+    email=Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    favorited_by_characters=relationship('FavoriteCharacters',back_populates='user')
+    favorited_by_planets=relationship('FavoritePlanets',back_populates='planets')
+    favorited_by_vehicles=relationship('Vehicles', secondary=favorited_vehicles, back_populates='favorited_by')
+
+class Characters(Base):
+    __tablename__='character'
+    id=Column(Integer,primary_key=True)
+    name=Column(String(250))
+    color_hair=Column(String(250))
+    age=Column(Integer)
+    height=Column(Integer)
+    favorite_characters=relationship('FavoriteCharacters',back_populates='character')
+    
+    
+class Planets(Base):
+    __tablename__='planets'
+    id=Column(Integer,primary_key=True)
+    name=Column(String(250))
+    atmosphere=Column(String(250))
+    diameter=Column(Integer)
+    rotation=Column(Integer)
+    orbital=Column(Integer)
+    favorite_planets=relationship('FavoritePlanets',back_populates='planets')
+
+class Vehicles(Base):
+    __tablename__='vehicles'
+    id=Column(Integer,primary_key=True)
+    model=Column(String(250))
+    vehicles=Column(String(250))
+    length=Column(String(250))
+    crew=Column(Integer)
+    passengers=Column(Integer)
+
+    favorite_vehicles=relationship('Vehicles',secondary=favorited_vehicles,back_populates='favorite_vehicles')
+
 
     def to_dict(self):
         return {}
